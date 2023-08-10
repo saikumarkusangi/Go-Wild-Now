87a3d51ddfe02dea46c92e9c97477d51b71cc1b9
NaveenKusangi14 <NaveenKusangi14@gmail.com>
2023-08-10 14:35:14 +0530

    footer

--- src/component/Layout/Footer.js
+++ src/component/Layout/Footer.js
@@ -6,8 +6,8 @@ import images from '../../constants/images';
 function Footer() {
   return (
     <div className='bg-black text-white overflow-hidden'>
-      <div className='flex flex-auto  py-2 w-1/2  '>
-        <div className='flex flex-col w-[55%]  px-[50%]   gap-4'>
+      <div className='flex flex-auto   py-4 md:w-1/2   '>
+        <div className='flex flex-col md:w-[55%]   md:px-[59%] px-10  gap-4'>
           <h1 className='text-xl text-red-700 font-Oswald font-bold'>Company</h1>
           <a className='text-lg' href='/'>Home</a>
           <a className='text-lg' href='/'>Location</a>
@@ -15,20 +15,21 @@ function Footer() {
           <a className='text-lg' href='/'>Shop</a>
           <a className='text-lg' href='/'>Contact</a>
         </div>
-        <div className=' flex flex-col  gap-4   '>
-          <h1 className='text-xl text-red-700 font-Oswald font-bold  '>Industries</h1>
+        <div className=' flex flex-col md:px-0 px-10 gap-4   '>
+          <h1 className='text-xl text-red-700 font-Oswald font-bold '>Industries</h1>
           <p className='text-lg'>Privacy Policy</p>
           <p className='text-lg'>Policies & Rules</p>
           <p className='text-lg'>Payment</p>
           <p className='text-lg'>Our Team</p>
-          <p className='text-lg'>Email: Devigo@gmail.Com</p>
+          <p className='text-lg'>Devigo@gmail.Com</p>
         </div>
+        
       </div>
       
-      <div className='px-10 flex flex-col gap-4 md:w-[50%] md:ml-[24%] mt-4 '>
-        <h1 className='md:text-4xl text-2xl font-bold ml-14 '>Subscribe To Our Newsletter</h1>
-        <input className='px-10 text-xl text-black font-medium py-1 ml-8 rounded-full' text='email' placeholder='Enter Your Email' />
-        <button className='bg-[#f9430a] font-lg px-4 py-2 rounded-full  hover:bg-red-500 ml-8'>Submit</button>
+      <div className='px-10 flex flex-col gap-4 md:w-[50%] md:ml-[25%] mt-4 '>
+        <h1 className='md:text-4xl text-xl font-bold md:ml-24 ml-6 '>Subscribe To Our Newsletter</h1>
+        <input className='px-10 text-xl text-black font-medium py-1  md:ml-8 rounded-full' text='email' placeholder='Enter Your Email' />
+        <button className='bg-[#f9430a] font-lg px-4 py-2 rounded-full  hover:bg-red-500 md:ml-8'>Submit</button>
       </div>
         <h1 className='md:text-xl text-red-800 font-Oswald font-bold  md:ml-[35%] mt-4 underline-white text-sm ml-[10%] mb-10'> Created by <span className='text-white'>Devigo Designer</span> | All right Reserved © 2023 </h1>
       
--- src/pages/Camping.js
+++ src/pages/Camping.js
@@ -43,10 +43,9 @@ const data = [
 ]
 
   return (
-
-    <div className='relative cursor-pointer '> 
+    <div className='relative cursor-pointer overflow-hidden '> 
     <img className='object-cover h-[60vh] md:h-full  md:w-full ' src={images.Abouth}alt='hrader'/>
-<div className="z-0  absolute top-[10%]  md:bg-transparent gap-4 md:left-[40%] flex flex-col md:gap-8 ">
+<div className="z-0  absolute top-[10%]  md:bg-transparent gap-4 md:left-[38%] flex flex-col md:gap-8 ">
      <h1 className="text-[#f8fbfe] text-2xl md:text-6xl font-bold md:px-10 px-24 ">Case Studies</h1>
  <div className='flex  justify-center gap-10 rounded-full py-4 ml-10    bg-white '>
      <a href='/' className='text-xl font-Oswald font-semibold hover:text-red-600   md:pl-8'>Home </a>
@@ -54,18 +53,17 @@ const data = [
      <a href='#' className='text-xl font-Oswald font-semibold text-[#f9430a] pr-4 '>Camping</a>
  </div>
   </div>
-  <div className='flex  flex-wrap md:w-[80%]  items-center  mx-auto my-10'> 
+  <div className='flex  flex-wrap md:w-[100%]  mx-12  md:mx-14 my-4 '> 
      {data.map(iteams => {
       return(
-          <div className=' md:w-[30%] mx-16  relative w-[160%] gap-8 md:mx-4 my-4'>
-            <img className='  md:h-full object-[40%] h-[40vh]  object-cover  rounded-lg  ' src={iteams.image} alt=''/>
+          <div className=' md:w-[30%]   relative  gap-8  mx-2 my-4'>
+            <img className=' md:h-full h-[40vh]  object-cover rounded-lg  ' src={iteams.image} alt='image'/>
             
             <div className='absolute left-0 bottom-0 md:bg-gradient-to-t from-[#000000d4] w-full h-full  '>
             <h1 className='text-2xl text-[#f36525] font-bold font-Oswald absolute bottom-20 left-5 md:left-10'>{iteams.Title}</h1>
             <p className='text-lg font- font-semibold text-white font-serif absolute bottom-10 left-5 md:left-10' >{iteams.name}</p>  
             <BsFillArrowRightCircleFill size={38} className='hover:animate-bounce text-white hover:text-[#f36525]  absolute left-[68%] md:left-[85%] bottom-8' />
-          
-        
+    
             </div>
          
           </div>
